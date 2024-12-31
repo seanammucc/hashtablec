ec052053465aaf9585cd05a043ba42dd1d7c5994
seanammucc <123333313@umail.ucc.ie>
2024-12-31 14:35:50 +0000

    s

--- hello.c
+++ hello.c
@@ -1,25 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void write_record(const char *filename, const char *record){
-    FILE *file = fopen(filename, "a");
-    if (file == NULL){
-        printf("Error opening file!\n");
-        return;
+#define TABLE_SIZE 10
+
+typedef struct Node {
+    char *key;
+    char *value;
+    struct Node *next;
+} Node;
+
+typedef struct HashTable {
+    Node *buckets[TABLE_SIZE];
+} HashTable;
+
+Node *create_node(char *key, char *value){
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    new_node->key = key;
+    new_node->value = value;
+    new_node->next = NULL;
+    return new_node;
+};
+
+unsigned int hash(char *key) {
+    unsigned int hash = 0;
+    for (int i = 0; i < strlen(key); i++) {
+        hash = key[i] + (hash << 6) + (hash << 16) - hash;
     }
-    fwrite(record, sizeof(char), strlen(record), file);
-    fclose(file);
+    return hash % TABLE_SIZE;
 }
 
-void read_record(const char *filename){
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL){
-        printf("Error opening file!\n");
+HashTable *create_table(){
+    HashTable *table = (HashTable *)malloc(sizeof(HashTable));
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table->buckets[i] = NULL;
+    }
+    return table;
+}
+
+void insert(HashTable *table, const char *key, const char *value){
+    unsigned int index = hash(key);
+    Node *node = create_node(key, value);
+
+    if (table->buckets[index] == NULL) {
+        table->buckets[index] = node;
+    } else {
+        Node *current = table->buckets[index];
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = node;
+    }
+};
+
+char *search (HashTable *table, const char *key){
+    unsigned int index = hash(key);
+    Node *current = table->buckets[index];
+    
+    while(current){
+        if (strcmp(current->key == key) == 0){
+            return current->value;
+        }
+        current = current->next;
+    }
+    return NULL;
+};
+
+void delete(HashTable *table, const char key){
+    unsigned int index = hash(key);
+    Node *current = table->buckets[index];
+
+    while (current != NULL && strcmp(current->key, key) != 0) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("Key not found\n");
         return;
     }
-    char buffer[256];
-    while(fgets(buffer, sizeof(buffer), file) != NULL){
-        printf("%s", buffer);
+
+    if (prev == NULL) {
+        table->buckets[index] = current->next;
+    } else {
+        prev->next = current->next;
     }
-    fclose(file);
-}
+
+    free(current->key);
+    free(current->value);
+    free(current);
+
+};
+
